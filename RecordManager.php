79212f42210549c826172a61f44edb1d910122ed
Nick Kiermaier <nick.k@rxmg.com>
2016-12-24 19:20:23 -0800

    upd"

---
+++ RecordManager.php
@@ -0,0 +1,33 @@
+<?php
+
+class RecordManager{
+
+    public function getColumnValues($field, $twoDimArray){
+            return array_column($twoDimArray, $field);
+        }
+
+    public function getColumnValuesUnique($field, $twoDimArray){
+        $fields =  array_column($twoDimArray, $field);
+        $out = array_keys(array_flip($fields));
+        return $out;
+    }
+
+    public function makeArray2DUnique($twoDimArray){
+        $out = [];
+        $keys = array_keys($twoDimArray[0]);
+        foreach($keys as $key){
+            $out[$key] = $this->getColumnValuesUnique($key, $twoDimArray);
+        }
+        return $out;
+    }
+
+    public function selectArrayRows($key, $value, $twoDimArray){
+        $out = [];
+        foreach($twoDimArray as $array){
+            if($array[$key] == $value){
+                $out[] = $array;
+            }
+        }
+        return $out;
+    }
+}
---
+++ RecordManagerTest.php
@@ -0,0 +1,110 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once "./RecordManager.php";
+
+class RecordManagerTest extends TestCase
+{
+    private $manager;
+
+    protected function setUp()
+    {
+        $this->manager = new RecordManager();
+    }
+
+    public function testGetColumnValues()
+    {
+        $array = [
+            ["car" => "chevy", "fruit"=> "apple", "vegetable" =>"spinach"],
+            ["car"=>"ford", "fruit"=>"pear", "vegetable" => "kale"],
+            ["car" => "bmw", "fruit"=> "banana", "vegetable" =>"corn"],
+            ["car"=>"jeep", "fruit"=>"orange", "vegetable" => "peas"]
+        ];
+
+        $truth = [
+            'chevy',
+            'ford',
+            'bmw',
+            'jeep'
+        ];
+
+        $results = $this->manager->getColumnValues('car', $array);
+        $this->assertEquals($results, $truth);
+    }
+
+    public function testgetColumnValuesUnique()
+    {
+        $array = [
+            ["car" => "chevy", "fruit"=> "apple", "vegetable" =>"kale"],
+            ["car"=>"chevy", "fruit"=>"pear", "vegetable" => "kale"],
+            ["car" => "bmw", "fruit"=> "pear", "vegetable" =>"corn"],
+            ["car"=>"jeep", "fruit"=>"orange", "vegetable" => "kale"]
+        ];
+
+        $truth = [
+            'chevy',
+            'bmw',
+            'jeep'
+        ];
+
+        $results = $this->manager->getColumnValuesUnique('car', $array);
+        $this->assertEquals($results, $truth);
+
+        $truth = [
+            'apple',
+            'pear',
+            'orange'
+        ];
+
+        $results = $this->manager->getColumnValuesUnique('fruit', $array);
+        $this->assertEquals($results, $truth);
+
+        $truth = [
+            'kale',
+            'corn'
+        ];
+
+        $results = $this->manager->getColumnValuesUnique('vegetable', $array);
+        $this->assertEquals($results, $truth);
+    }
+
+    public function testMakeArray2DUnique()
+    {
+        $array = [
+            ["car" => "chevy", "fruit"=> "apple", "vegetable" =>"kale"],
+            ["car"=>"chevy", "fruit"=>"pear", "vegetable" => "kale"],
+            ["car" => "bmw", "fruit"=> "pear", "vegetable" =>"corn"],
+            ["car"=>"jeep", "fruit"=>"orange", "vegetable" => "kale"]
+        ];
+
+        $truth = [
+            "car" => ["chevy", "bmw", "jeep"],
+            "fruit"=>["apple","pear", "orange"],
+            "vegetable" =>["kale", "corn"]
+        ];
+
+        $results = $this->manager->makeArray2DUnique($array);
+        $this->assertEquals($results, $truth);
+    }
+
+    public function testSelectArrayRows()
+    {
+        $array = [
+            ["car" => "chevy", "fruit"=> "apple", "vegetable" =>"kale"],
+            ["car"=>"chevy", "fruit"=>"pear", "vegetable" => "kale"],
+            ["car" => "bmw", "fruit"=> "pear", "vegetable" =>"corn"],
+            ["car"=>"jeep", "fruit"=>"orange", "vegetable" => "kale"]
+        ];
+
+        $truth = [
+            ["car" => "chevy", "fruit"=> "apple", "vegetable" =>"kale"],
+            ["car"=>"chevy", "fruit"=>"pear", "vegetable" => "kale"]
+        ];
+
+        $results = $this->manager->selectArrayRows('car', 'chevy', $array);
+        echo var_dump($results);
+        echo var_dump($truth);
+        $this->assertEquals($results, $truth);
+    }
+
+}
